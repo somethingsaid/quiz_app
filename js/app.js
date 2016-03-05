2dd3bb181271a919cbe2d4d928aab117f7508d12
somethingsaid <kwong.billy@gmail.com>
2016-03-05 11:51:49 -0500

    HTML done, working on CSS and JavaScript

---
+++ js/app.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+/*--- Global variables ---*/ 
+var numberCorrect = 0;
+var currentQuestion = 0;
+
+/*--- Bouldering quiz object ---*/
+var boulderQuestions = [
+{
+    questionNum: 0,
+    questionText: "What does FFA stand for?",
+    choices: ["Fastest Free Ascent", "First Free Ascent", "First Foreign Ascent", "Fastest Foreign Ascent"],
+    correct: 1,
+    fact: "FFA could also stand for First Female Ascent"
+},
+{
+    questionNum: 1,
+    questionText: "ABO means... ",
+    choices: ["Always Be Outdoors", "Assisted Brake Option", "Abominable", "Analysis of Belay Outcome"],
+    correct: 2,
+    fact: "Abominable is the top rating on the French Alpine grading scale"
+},
+{
+	questionNum: 2,
+    questionText: "A favourite retailer of outdoor recreation gear in Canada, MEC stands for",
+    choices: ["Mountain Equipment Co-op", "Mountain Equipment Co.", "Mount Edmonton Clothiers", "Mecca Excursion Center"],
+    correct: 0,
+    fact: "MEC is notable for its commitment to environmental protection"
+},
+{
+	questionNum: 3,
+    questionText: "The common \"V\" scale for grading the difficulty of a bouldering problem is als called",
+    choices: ["Vertical Scale", "Biller's System", "Vermin's Grade", "Hueco Scale"],
+    correct: 3,
+    fact: "Created by John \"Vermin\" Sherman in the 1990s, this is the most widely used system in North America"
+},
+{
+	questionNum: 4,
+    questionText: "Beta refers to",
+    choices: ["a test route when designing competitions", "the second person to ascent", "plan B", "advice on how to complete a route"],
+    correct: 3,
+    fact: "There can be multiple betas.  Some believe that providing betas 'taints' an ascent."
+}]
+
+/*--- Declare functions ---*/
+
+/*--- Event Listeners ---*/
+
+
+
+});
